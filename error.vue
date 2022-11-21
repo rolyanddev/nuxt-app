567b5b78b63371e657e20114dd41b75677ebcaa8
RolyDev <rolyanddev@gmail.com>
2022-11-21 14:44:44 -0500

    added custom error page

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="h1">Error {{ error.statusCode }}, {{ error.message }} -_-</h1>
+  <button @click="handleClearError">Ir al inicio</button>
+</template>
+
+<script setup lang="ts">
+defineProps(['error'])
+
+const handleClearError = () => clearError({ redirect: '/' })
+</script>
