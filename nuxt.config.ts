0c8928daa81ad87bc5ce24890668f571a359900b
RolyDev <rolyanddev@gmail.com>
2022-11-21 14:45:53 -0500

    added head config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,18 @@ import { User, Users } from './interfaces/Users'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      titleTemplate: `%s - Nuxt App Example`,
+      meta: [{ name: 'author', content: 'RolyDev' }],
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone'
+        }
+      ]
+    }
+  },
   hooks: {
     async 'nitro:config'(nitroConfig) {
       if (nitroConfig.dev) {
