a3a053f6db64f2f03276b774881d63d1254648f1
RolyDev <rolyanddev@gmail.com>
2022-11-21 14:46:59 -0500

    create error

--- pages/user/[id]/index.vue
+++ pages/user/[id]/index.vue
@@ -14,11 +14,18 @@ import { User } from '~~/interfaces/Users'
 
 const route = useRoute()
 
+const uri = `https://jsonplaceholder.typicode.com/users/${route.params.id}`
+
 definePageMeta({
   layout: 'users'
 })
 
-const { data: user } = await useFetch<User>(
-  `https://jsonplaceholder.typicode.com/users/${route.params.id}`
-)
+const { data: user } = await useFetch<User>(uri)
+
+if (!user.value) {
+  throw createError({
+    statusCode: 404,
+    message: 'Página de usuario not found'
+  })
+}
 </script>
